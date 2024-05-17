5b568f3247e60e0c917c623ed1f57565b62073c6
novaingh <akashdeep@hiehq.com>
2024-05-17 15:29:46 +1000

    Add Hero Section

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,4 +1,21 @@
+import { BsArrowRight } from "react-icons/bs";
+import PokerImg from "../../assets/images/istockphoto-628650056-612x612-1.jpg";
 const HeroSection = () => {
-    return 'Hero Section';
+    return (
+        <div className="hero-section">
+            <div className="bg-overlay-hero"></div>
+            <div className="container text-light d-flex justify-content-between position-relative z-1 flex-column-reverse flex-lg-row">
+                <div className="w-50">
+                    <h1>MOST TRUSTED ONLINE RUMMY SITE</h1>
+                    <p className="text-orange">SKYYEET SOFTWARE TECHNOLOGY PRIVATE LIMITED</p>
+                    <p>Our website is one of the most exciting rummy websites to play all kinds of rummy games online. where you can enjoy online rummy with your friends</p>
+                    <button className="btn download-app">Download App <BsArrowRight className="ms-2" /></button>
+                </div>
+                <div className="w-50">
+                    <img src={PokerImg} className="img-fluid" alt="Poker Img"/>
+                </div>
+            </div>
+        </div>
+    );
 }
 export default HeroSection;
