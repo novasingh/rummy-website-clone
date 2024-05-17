c5b6289f9b7d52c965ab72e141c87f3b7e7928e6
novaingh <akashdeep@hiehq.com>
2024-05-17 16:44:11 +1000

    Add Card Section

--- src/components/DownloadBtn.js
+++ src/components/DownloadBtn.js
@@ -1,8 +1,14 @@
 import { BsArrowRight } from "react-icons/bs";
 
-const DownloadBtn = ({btnClass , text = "Download App" , iconEnable = true}) => {
-    return (
-        <button className={`btn download-app  ${btnClass}`}>{text} {iconEnable && <BsArrowRight className="ms-2" />}</button>
-    )
-}
+const DownloadBtn = ({
+  btnClass,
+  text = "Download App",
+  iconEnable = true,
+}) => {
+  return (
+    <button className={`btn download-app  ${btnClass}`}>
+      {text} {iconEnable && <BsArrowRight className="ms-2" />}
+    </button>
+  );
+};
 export default DownloadBtn;
--- src/components/sections/CardSection.js
+++ src/components/sections/CardSection.js
@@ -1,4 +1,30 @@
 const CardSection = () => {
-    return 'Card Section';
-}
+  const content = [
+    {
+      id: 1,
+      title: "RESPONSIBLE PLAY",
+      description:
+        "To make sure your experience is as enjoyable and lucrative, we limit the number of times you can play real money rummy games.",
+    },
+    {
+      id: 2,
+      title: "100% LEGAL",
+      description:
+        "Rummy was declared to be a game of skill by the Honbl. Supreme Court of India in 1968 and is 100% legal to play for free or cash.",
+    },
+  ];
+
+  return (
+    <div className="card-section">
+      <div className="container d-flex flex-column flex-lg-row">
+        {content.map((item, index) => (
+             <div key={index+'cards'} className="card-section--box">
+             <h1>{item.title}</h1>
+             <p>{item.description}</p>
+           </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default CardSection;
